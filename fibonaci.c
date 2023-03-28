496d0d6f2ee24f4ee0417a5aadcf6bb50268e033
Sayanjit Mukherjee <sayanjitmukherjee123@gmail.com>
2023-03-28 21:19:51 +0530

    changing the prog

--- fibonaci.c
+++ fibonaci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 void main(){
-    int a=0,b=1,c=0,n=10,i;
+    int a=0,b=1,c=0,n=20,i,even=0,odd=0;
     printf("%d",a);
     printf(" ");
     printf("%d",b);
@@ -12,5 +12,15 @@ void main(){
         b=c;
         printf("%d",c);
         printf(" ");
+        if(i%2==0){
+            even=even+c;
+        }
+        else{
+            odd=odd+c;
+        }
     }
+    printf("\n");
+    printf("%d",even);
+    printf("\n");
+    printf("%d",odd);
 }
---
+++ forloop.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+void main(){
+    int ar[20];
+    int even[10];
+    int odd[10];
+    int k=0,i
+    for(i=0;i<20;i++,k++){
+        if(ar[i]%2==0){
+            even[k]=ar[i];
+        }else{
+            odd[k]=ar[i];
+        }
+        
+    }
